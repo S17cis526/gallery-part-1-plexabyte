309b066abe2e402ccaa9855137a2ea5804bca5f3
plexabyte <zmarcolesco@ksu.edu>
2017-01-26 13:50:10 -0600

    Added basic server function.

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
+"use strict";
+
 /**
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
  */
- 
+
+var http = require('http');
+var fs = require('fs');
+var port = 3000; //80 usually for development, requires admin
+
+var server = http.createServer((req, res) => {
+
+  switch(req,url) {
+    case "/chess":
+    var body = fs.readFileSync('images/chess.jpg');
+    res.setHeader("Content-Type", "image/jpeg");
+    res.end(body);
+    break;
+  default:
+    res.statusCode = 404;
+    res.statusMessage = "Not Found";
+  }
+
+});
+
+server.listen(port, ()=> {
+	console.log("Listening on Port " + port);
+});
