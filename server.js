544f84fac977580d455c07a770140ae306c2892b
Zach Marcolesco <zach.marcolesco@gmail.com>
2017-02-21 22:51:13 -0600

    Added some more javascript functionality and CSS looks.

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ var port = 3000;
 
 /* load cached files */
 var config = JSON.parse(fs.readFileSync('config.json'));
-var stylesheet = fs.readFileSync('gallery.css');
+var stylesheet = fs.readFileSync('public/gallery.css');
+var script = fs.readFileSync("public/gallery.js")
 
 /* load templates */
 template.loadDir('templates');
@@ -25,7 +26,7 @@ template.loadDir('templates');
  * Retrieves the filenames for all images in the
  * /images directory and supplies them to the callback.
  * @param {function} callback - function that takes an
- * error and array of filenames as parameters
+ * error and array of filenames as parameterss
  */
 function getImageNames(callback) {
   fs.readdir('images/', function(err, fileNames){
@@ -182,6 +183,10 @@ function handleRequest(req, res) {
     case '/gallery.css':
       res.setHeader('Content-Type', 'text/css');
       res.end(stylesheet);
+      break;
+    case '/gallery.js':
+      res.setHeader('Content-Type', 'text/javascript');
+      res.end(script);
       break;
     default:
       serveImage(req.url, req, res);
